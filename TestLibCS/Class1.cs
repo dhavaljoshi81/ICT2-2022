197002ad5358bf29df6810096536801d4ddd4e7c
Dr. Dhaval Joshi <59249502+dhavaljoshi81@users.noreply.github.com>
2022-03-23 11:09:08 +0530

    Add files via upload

---
+++ TestLibCS/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibCS
+{
+    public class Student
+    {
+        public Student()
+        {
+
+        }
+        private int StudID{ get; set; }
+        public string Name { get; set; }
+        protected int Age { get; set; }
+    }
+}
